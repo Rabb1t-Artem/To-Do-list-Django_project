9f91d02b293d91ff30334adef1cc64019b3665ec
Artem Shatylo <Alukard32244@gmail.com>
2024-11-27 16:32:55 +0200

    Refactor TaskForm

--- to_do_list/forms.py
+++ to_do_list/forms.py
@@ -5,12 +5,15 @@ from to_do_list.models import Task
 
 
 class TaskForm(forms.ModelForm):
-    deadline = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}))
-    tags = forms.ModelMultipleChoiceField(
-        queryset=get_user_model().objects.all(),
-        widget=forms.CheckboxSelectMultiple
-    )
 
     class Meta:
         model = Task
-        fields = ["content", "deadline", "tags"]
+        fields = ("content", "deadline", "tags")
+        widgets = {
+            "content": forms.Textarea(attrs={"rows": 3}),
+            "deadline": forms.DateInput(
+                format="%Y-%m-%d",
+                attrs={"placeholder": "Select a date", "type": "date"},
+            ),
+            "tags": forms.CheckboxSelectMultiple(),
+        }
